2d1c3703f5475ead1908c4c7e2ad8e4ecde92b34
Arti Biradar <140704728+artibiradar13@users.noreply.github.com>
2024-01-30 16:00:43 +0530

    Add files via upload

---
+++ main1.java
@@ -0,0 +1,40 @@
+public class main1{
+    public static void main(String[] args) {
+       //variables-to store temporary data in memory
+         String name="Aman";
+        int age= 30;
+
+        //data types 
+        /*primitive data type
+         * byte
+         * short
+         * int
+         * long
+         * float
+         * double
+        */
+        byte age1=30;
+        int phone= 123456566;
+        long telephone=23456775L;
+        float pi=3.14F;
+
+        /*non-primitive -strings */
+        String Name="Leo";
+        System.out.println(Name.length());
+        //concatenate
+        String name1="Vara";
+        String name2="Sar";
+        String fullname=name1+" "+name2; 
+        System.out.println(fullname);
+        System.out.println(name1.charAt(0));
+        String change= name1.replace('a', 'b');
+        System.out.println(change);
+
+
+        
+        
+
+
+
+    }
+}
