e667f75fbb344faf480017be567500ffce94319f
= <=>
2024-02-02 22:44:44 +0530

    arrays

---
+++ arrays.java
@@ -0,0 +1,10 @@
+public class arrays {
+    // ARRAYS- collection of similar datatype 
+    public static void main(String[] args) {
+        //syntax
+        // int[] marks=  new int[5];
+        int marks[]=new int[]{1,2,3,4,5};
+        String[] arr= new String[]{aa,bb,cc};
+    }
+    
+}
